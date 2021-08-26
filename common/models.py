2332cd342208b3d86932c4b4b32ce3d8b510c784
Heeyoung Joe <HeeyoungJoe2000@gmail.com>
2021-08-26 21:46:56 +0900

    260926 heeyoung user

---
+++ common/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from common import forms
+
+
+class User(AbstractUser):
+    #pre-set
+
+    username = models.CharField(max_length=10,unique=True)
+    password = models.CharField(max_length=100,default="")
+
+    #optional
+    email=models.EmailField(_('email address',widget=forms.EmailInput({'placeholder': 'test@example.com'})),unique=True)
+
+    USERNAME_FIELD='username'
+    PASSWORD_FIELD='password'
+    REQUIRED_FIELDS=['email']
+
+    phone = models.CharField(max_length=20,default=None)
+    address = models.CharField(max_length=50,default=None)
+    # Account = models.CharField(max_length=50)
+    balance = models.CharField(max_length=50,default=None)
+    create_date = models.DateTimeField(default=timezone.make_aware(timezone.datetime.max, timezone.get_default_timezone()))
+    # record = models.OnetoOneField(Record, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.email
+'''
+class User(models.Model):
+    pass
+
+#create test user --debug
+user=User.objects.create_user('green','admin@greenbalm.com','balm')
+user.first_name='yeonkoung'
+user.last_name='kim'
+user.save()
+'''

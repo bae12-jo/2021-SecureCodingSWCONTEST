227fb746364674056ad31be1ee59f83d6f207987
Heeyoung Joe <HeeyoungJoe2000@gmail.com>
2021-08-26 15:03:39 +0900

    260303 heeyoung new

---
+++ group/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm,Textarea
+from django.utils.translation import gettext_lazy as _
+from .models import Group
+def GroupForm(ModelForm):
+    class Meta:
+        model=Group
+        fields=('group_name','deposit','fine','reward_method','teammates','rules')
+        widgets={
+            'rules':Textarea(attrs={'cols':80,'rows':20}),
+        }
+        labels={
+            'group_name':_('Group Name'),
+            'deposit':_('Required Deposit'),
+            'fine':_('Fine'),
+            'reward_method':_('Choose how to distribute reward'),
+            'teammates':_('Input ID of your teammates'),
+            'rules':_('Rules of this study group')
+        }
+
+

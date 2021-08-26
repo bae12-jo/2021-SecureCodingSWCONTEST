a08128eb59a8499e0029be74cfeec2b41b8a7508
Eunbi Choi <78725389+eunbi816@users.noreply.github.com>
2021-08-26 06:37:30 +0000

    Create my_settings.py

---
+++ db_settings/my_settings.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studymanage_db',
+        'USER': 'root',
+        'PASSWORD': 'green',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
+
+SECRET_KEY = 'django-insecure-c%f8&zc*__p0r5(zbz8lm@kka-0m1y-bm@2q48nz@mrlmd1(x)'

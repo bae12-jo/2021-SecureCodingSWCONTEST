4753303d7673e4e1b990490cb71cd2eff1e20fd1
Heeyoung Joe <HeeyoungJoe2000@gmail.com>
2021-08-27 10:55:27 +0900

    271055 heeyoung added

--- group/models.py
+++ group/models.py
@@ -1,6 +1,8 @@
 from django.db.models import ManyToManyField
 from django.db import models
 from django.urls import reverse
+from django import forms
+from common.models import User
 # Create your models here.
 class Group(models.Model):
     #이름
@@ -13,27 +15,26 @@ class Group(models.Model):
         (2,' All to the single winner'),
     ]
     reward=models.CharField(choices=REWARD_CHOICES, max_length=100,null=True)
-
-    #detail view를 위한 slug
-    slug=models.SlugField(max_length=200,db_index=True,unique=True, allow_unicode=True)
-
+    money_sum=models.FloatField(null=True)#벌금 모음
     #사람
-
-    teammate=ManyToManyField(
+    '''
+    potential_teammate=ManyToManyField(
         'common.User',
         related_name='teammates',
         related_query_name='teammate',
 
     )
-    leader=models.ForeignKey(
-        'common.User',
-        on_delete=models.CASCADE,
-        default=None,
-    )
+    '''
+    #potential_teammate=User.objects.all()
 
+    leader=models.CharField(max_length=100,default="Song")
+    teammate=models.CharField(max_length=100,null=True)
     #규칙
     rule=models.CharField(max_length=500,null=True)
-    can_pay=models.BooleanField(default=True,)
+
+    #leader가 group을 수정하는 동안은 결제 버튼을 누를 수 없다.
+    pemitted_to_pay=models.BooleanField(default=True)
+
     class Meta:
         ordering=['group_name']
         verbose_name='group'
@@ -42,23 +43,25 @@ class Group(models.Model):
         return self.group_name
     def get_absolute_url(self):
         return reverse('my_groups_detail',args=[str(self.group_name)])
-
+    def create(cls,group_name,deposit,fine,reward,leader,rule):
+        newgroup=cls(
+            group_name=group_name,
+            deposit=deposit,
+            fine=fine,
+            reward=reward,
+            leader=leader,
+            rule=rule
+        )
+        return newgroup
 '''
-class Record(models.Model):
+#debug를 위한 group instance testing
+new_group=Group.create(
+    group_name='wake-up study',
+    deposit=10000,
+    fine=100,
+    reward=1,
+    leader="Joe",
+    rule="none",
 
-    associated_group=models.OneToOneField(
-        Group,
-        on_delete=models.CASCADE
-
-    )
-
-    associated_user=models.OneToOneField(
-        'common.User',
-        on_delete=models.CASCADE,
-
-    )
-
-    total_fine=models.FloatField()
-    fine_count=models.IntegerField()
-    '''
-
+)
+'''
